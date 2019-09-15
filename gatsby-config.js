75d59c0031e1f27df6a8cbe32ec6bc6fe161f069
Andrea Peretti <and.xet@gmail.com>
2019-09-15 20:59:35 +0200

    Added cookiehub

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const contentful = require('contentful');
 const manifestConfig = require('./manifest-config');
 require('dotenv').config();
 
-const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
+const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID, COOKIEHUB } = process.env;
 
 const client = contentful.createClient({
   space: SPACE_ID,
@@ -47,11 +47,32 @@ module.exports = client.getEntries().then(entries => {
     },
   });
 
-  if (ANALYTICS_ID) {
+  if (ANALYTICS_ID && false) {
     plugins.push({
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: ANALYTICS_ID,
+        anonymize: true,
+      },
+    });
+  }
+
+  if (ANALYTICS_ID && COOKIEHUB) {
+    console.log('Activating cookiehub');
+    plugins.push({
+      resolve: 'gatsby-plugin-cookiehub',
+      options: {
+        trackingId: ANALYTICS_ID,
+        anonymize: true,
+
+        // your cookiehub widget ID
+        cookihubId: COOKIEHUB,
+        // your google analytics tracking id
+        trackingId: ANALYTICS_ID,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     });
   }
